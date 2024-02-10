43fa4ca1d542b93cb1bac50b6f706adff368dc68
Kruglov Alexey <kruglov_aleksei@mail.ru>
2024-02-10 23:48:23 +0300

    feat: remove contracts validator

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,2 @@
 class ApplicationController < ActionController::API
-  before_action :validate_params!
-
-  rescue_from ContractValidationError do
-    render json: "Invalid input", status: :method_not_allowed
-  end
-
-  private
-
-  def validate_params!
-    controller = self.class.name.sub("Controller", "")
-    action = action_name.classify
-    "Contracts::#{controller}::#{action}".safe_constantize
-                                         &.new(params)
-                                         &.call
-  end
 end
--- lib/contracts/students/create.rb
+++
@@ -1,9 +0,0 @@
-class Contracts::Students::Create < Contracts::BaseContract
-  def schema
-    required(:first_name, :string)
-    required(:last_name, :string)
-    required(:surname, :string)
-    required(:klass_id, :integer)
-    required(:school_id, :integer)
-  end
-end
