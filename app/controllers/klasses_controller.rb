be825c9a430efd831fa933972cfebbe9a7552531
Kruglov Alexey <kruglov_aleksei@mail.ru>
2024-02-11 09:48:06 +0300

    feat: change actions according to requirements

--- app/controllers/klasses_controller.rb
+++ app/controllers/klasses_controller.rb
@@ -1,45 +1,7 @@
 class KlassesController < ApplicationController
-  before_action :set_klass, only: %i[show update destroy]
-
   def index
     @klasses = Klass.where(school_id: params[:school_id])
 
     render "klasses/index", status: :ok
-  end
-
-  def show
-    render json: @klass
-  end
-
-  def create
-    @klass = Klass.new(permitted_params)
-
-    if @klass.save
-      render json: @klass, status: :created
-    else
-      render json: @klass.errors, status: :unprocessable_entity
-    end
-  end
-
-  def update
-    if @klass.update(permitted_params)
-      render json: @klass
-    else
-      render json: @klass.errors, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @klass.destroy
-  end
-
-  private
-
-  def set_klass
-    @klass = Klass.find(params[:id])
-  end
-
-  def permitted_params
-    params.require(:klass).permit(:number, :letter, :school_id)
   end
 end
--- app/views/klasses/index.json.jbuilder
+++ app/views/klasses/index.json.jbuilder
@@ -2,5 +2,5 @@ json.data @klasses do |klass|
   json.id klass.id
   json.number klass.number
   json.letter klass.letter
-  json.students_count klass.students.count
+  json.students_count klass.students_count
 end
